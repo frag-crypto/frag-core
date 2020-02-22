81eaff2ef03f5c7f50ac82ab4b3e75ab879cce42
irontiga <irontiga@gmail.com>
2020-02-23 03:00:47 +1300

    prr

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ const coin = require('./default.coin.config.js')
 const crypto = require('./default.crypto.config.js')
 const user = require('./default.user.config.js')
 const styles = require('./default.styles.config.js')
-const build = require('./default.build.options')
+const build = require('./default.build.options.js')
 
 module.exports = { coin, crypto, user, styles, build }
